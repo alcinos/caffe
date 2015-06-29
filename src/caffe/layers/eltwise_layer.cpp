b9a0dcbdc9828c7df2fad494d18dc1dff0cfb134
alcinos <carion.nicolas@gmail.com>
2015-06-29 15:34:28 -0600

    typo3

--- src/caffe/layers/eltwise_layer.cpp
+++ src/caffe/layers/eltwise_layer.cpp
@@ -33,7 +33,7 @@ void EltwiseLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
   for (int i = 1; i < bottom.size(); ++i) {
     CHECK(bottom[i]->count() == bottom[0]->count());
-    if(bottom[i]->shape != bottom[0]->shape()){
+    if(bottom[i]->shape() != bottom[0]->shape()){
         if(bottom[0]->shape()[0]*bottom[0]->shape()[1] == bottom[0]->count()){
             CHECK(bottom[0]->shape()[0] == bottom[i]->shape()[0]);
             CHECK(bottom[0]->shape()[1] == bottom[i]->shape()[1]);
